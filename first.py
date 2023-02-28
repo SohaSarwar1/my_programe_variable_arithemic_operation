5b53906f9d8ac1c55b909f639937bf0405a5e66d
SohaSarwar1 <122516467+SohaSarwar1@users.noreply.github.com>
2023-02-28 13:01:14 +0500

    my first programme
    in this i am using the variable and find the type of variable and casting the variable value also practice of arithmetic operation which is adding , substation , division , multiplication ,   modules , floor division

---
+++ first.py
@@ -0,0 +1,50 @@
+var_a = 12
+var_b = 13
+
+print(var_a)
+print(var_b)
+
+var_c = 'i am pakistani'
+print(var_c)
+
+
+'''noe check the type of variable'''
+print(type(var_a))
+print(type(var_b))
+print(type(var_c))
+
+'''now using the type converstion'''
+var_d = str(var_a)
+print(var_d)
+print(type(var_d))
+
+'''assigent the multiple variable in single line'''
+var_e , var_f, var_g = 'punjab' , 'university', 'new campus'
+print(var_e)
+print(var_f)
+print(var_g)
+
+'''assign the single value in multiple variable in single line'''
+
+var_h = var_i = var_j = 'soha'
+print(var_h)
+print(var_i)
+print(var_j)
+print(type(var_h))
+print(type(var_i))
+print(type(var_j))
+
+
+'''adding the value using the arithemitic operation'''
+'''adding some value'''
+print(var_a + var_b)
+'''substration some value'''
+print(var_b - var_a)
+'''multiple some value'''
+print(var_a * var_b)
+'''division some value'''
+print(var_a / var_b)
+'''floor division some value'''
+print(var_a // var_b)
+'''modules some value'''
+print(var_a % var_b)
